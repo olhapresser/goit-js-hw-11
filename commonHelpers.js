22a65dc63efcdf8e4e59e559f3f99fd25ff65052
olhapresser <105518044+olhapresser@users.noreply.github.com>
2024-02-03 17:03:21 +0000

    Deploying to gh-pages from @ olhapresser/goit-js-hw-11@433c3ead586213b1d0cbb40f853f05e9f42f8fc6 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,23 +1,23 @@
-import{S as c,i as l}from"./assets/vendor-46aac873.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&a(i)}).observe(document,{childList:!0,subtree:!0});function n(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerpolicy&&(r.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?r.credentials="include":e.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function a(e){if(e.ep)return;e.ep=!0;const r=n(e);fetch(e.href,r)}})();const s={form:document.querySelector("#form"),input:document.querySelector(".input"),container:document.querySelector(".container"),loading:document.querySelector(".loading")},u=new c(".container a",{});s.form.addEventListener("submit",t=>{t.preventDefault();const o=t.currentTarget,n=o.elements.query.value;n.trim()&&(s.loading.style.display="block",m(n).then(g).catch(h).finally(()=>{o.reset(),s.loading.style.display="none"}))});const f="42114323-50c2b50fedad4076dc7ba42b5",d="https://pixabay.com/api/";function p(t){return`${d}?key=${f}&image_type=photo&orientation=horizontal&safesearch=true&q=${t}`}function m(t){const o=p(t);return fetch(o).then(n=>{if(!n.ok)throw new Error(n.statusText);return n.json()}).then(({hits:n})=>n)}function h(){l.error({title:"Sorry, something went wrong."})}function y(){l.error({title:"Sorry, there are no images matching your search query. Please try again!"})}function g(t){if(t.length===0)return s.container.innerHTML="",y();b(t),u.refresh()}function b(t){const o=t.map(n=>L(n)).join(" ");console.log(o),s.container.innerHTML=o}function L({webformatURL:t,largeImageURL:o,tags:n,likes:a,views:e,comments:r,downloads:i}){return`
+import{S as c,i as l}from"./assets/vendor-46aac873.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const n of e)if(n.type==="childList")for(const s of n.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&a(s)}).observe(document,{childList:!0,subtree:!0});function r(e){const n={};return e.integrity&&(n.integrity=e.integrity),e.referrerpolicy&&(n.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?n.credentials="include":e.crossorigin==="anonymous"?n.credentials="omit":n.credentials="same-origin",n}function a(e){if(e.ep)return;e.ep=!0;const n=r(e);fetch(e.href,n)}})();const i={form:document.querySelector("#form"),input:document.querySelector(".input"),container:document.querySelector(".container"),loading:document.querySelector(".loading")},u=new c(".container a",{});i.form.addEventListener("submit",t=>{t.preventDefault();const o=t.currentTarget,r=o.elements.query.value;r.trim()&&(i.loading.style.display="block",h(r).then(g).catch(m).finally(()=>{o.reset(),i.loading.style.display="none"}))});const p="42114323-50c2b50fedad4076dc7ba42b5",d="https://pixabay.com/api/";function f(t){return`${d}?key=${p}&image_type=photo&orientation=horizontal&safesearch=true&q=${t}`}function h(t){const o=f(t);return fetch(o).then(r=>{if(!r.ok)throw new Error(r.statusText);return r.json()}).then(({hits:r})=>r)}function m(){l.error({title:"Sorry, something went wrong."})}function y(){l.error({title:"Sorry, there are no images matching your search query. Please try again!"})}function g(t){if(t.length===0)return i.container.innerHTML="",y();b(t),u.refresh()}function b(t){const o=t.map(r=>_(r)).join(" ");console.log(o),i.container.innerHTML=o}function _({webformatURL:t,largeImageURL:o,tags:r,likes:a,views:e,comments:n,downloads:s}){return`
     <a href="${o}">
-    <div>
-    <img src="${t}" alt="${n}">
+    <div class="card">
+    <img src="${t}" alt="${r}">
 <ul class="photo_details">
 <li>
-<span>Likes</span><br>
+<span class="photo_details_paragraph">Likes</span><br>
 <span>${a}</span>
 </li>
 <li>
-<span>Views</span><br>
+<span class="photo_details_paragraph">Views</span><br>
 <span>${e}</span>
 </li>
 <li>
-<span>Comments</span><br>
-<span>${r}</span>
+<span class="photo_details_paragraph">Comments</span><br>
+<span>${n}</span>
 </li>
 <li>
-<span>Downloads</span><br>
-<span>${i}</span>
+<span class="photo_details_paragraph">Downloads</span><br>
+<span>${s}</span>
 </li>
 </ul>
     </div>
